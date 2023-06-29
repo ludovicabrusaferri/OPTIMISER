9791ed5724f2b6c6b7580c9bd0f2ed6ae12ab5c2
ludovicabrusaferri <loginvarious12@gmail.com>
2023-06-29 16:34:08 -0400

    sampling

--- sampling.py
+++ sampling.py
@@ -1,25 +1,25 @@
 import numpy as np
 from scipy.special import expit
 
-def generate_samples(alpha, beta, mixture_weights, num_samples):
-    num_components = len(mixture_weights)
+def generate_samples(beta, alpha, num_samples):
+    num_components, num_features = beta.shape
 
     samples = []
     for _ in range(num_samples):
-        # Step 4: Generate random input variables
-        random_input = np.random.randn(alpha.shape[1])  # Assuming standard normal distribution
+        # Step 1: Assign samples to components based on mixture weights
+        component = np.random.choice(num_components, p=alpha)
 
-        # Step 5: Calculate log-odds for each component
-        log_odds = [np.dot(random_input, beta_comp.T) for beta_comp in beta]
+        # Step 2: Generate random input variables
+        random_input = np.random.randn(num_features)  # Assuming standard normal distribution
 
-        # Step 6: Convert log-odds to probabilities for each component using the logistic function
-        probabilities = [expit(log_odds_comp) for log_odds_comp in log_odds]
+        # Step 3: Calculate log-odds for the selected component
+        log_odds = np.dot(random_input, beta[component])
 
-        # Step 7: Assign samples to components based on mixture weights
-        component = np.random.choice(num_components, p=mixture_weights)
+        # Step 4: Convert log-odds to probabilities using the logistic function
+        probability = expit(log_odds)
 
-        # Step 8: Generate samples for the assigned component
-        sample = np.random.uniform() <= probabilities[component]
+        # Step 5: Generate samples based on the probability
+        sample = np.random.uniform() <= probability
         samples.append(sample)
 
     return samples

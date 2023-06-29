f15c3858b4b360be2d0883a98f02d7ef00d17549
ludovicabrusaferri <loginvarious12@gmail.com>
2023-06-29 14:11:43 -0400

    fixed

--- main.py
+++ main.py
@@ -41,15 +41,20 @@ def loglikelihoodlogisticregression(meas,model,beta):
     return np.sum(meas*beta*model - np.log(1 + np.exp(beta*model)))
 
 def loglikelihoodmixturelogisticregression(meas,model,beta,alpha):
-    return np.sum(np.sum(alpha*(meas*beta*model - np.log(1 + np.exp(beta*model)))))
+    # https://arxiv.org/pdf/1802.10529.pdf
+    # alpha and beta must have the same lenght
+    temp=np.zeros(meas.shape)
+    for k in range(len(alpha)):
+        temp+=alpha[k]*(meas*beta[k]*model - np.log(1 + np.exp(beta[k]*model)))    
+    return np.sum(temp)
 
 
 #value = KLgaussian(0,0,1,1)
 #value = KLgaussianForMeanZeroAndStdOne(0,1)
 meas=np.array([1, 2, 3])
 model=np.array([1, 2, 3])
-beta=np.array([1, 1, 3])
-alpha=np.array([1, 1, 3])
+beta=np.array([1, 1])
+alpha=np.array([1, 1])
 value = loglikelihoodmixturelogisticregression(meas,model,beta,alpha)
 print(value)
 

d51e2f8416eb9e9290e09bc369d821aea8616dff
ludovicabrusaferri <loginvarious12@gmail.com>
2023-07-01 09:30:00 -0400

    sampling

--- sampling.py
+++ sampling.py
@@ -3,18 +3,19 @@ import numpy as np
 def sampling_function(meas, model, N_samples, beta, alpha=None, threshold=None):
     if alpha is None:
         # Sample from a logistic distribution
-        samples = np.random.logistic(scale=beta * model, size=N_samples)
+        samples = np.random.logistic(loc=model, scale=beta, size=N_samples)
     else:
         if threshold is None:
-            # Sample from a mixture logistic distribution
-            component_indices = np.random.choice(len(alpha), size=N_samples, p=alpha)
-            component_samples = np.random.logistic(scale=beta * model)
-            samples = component_samples[component_indices]
+            # Sample from a mixture of logistic distributions
+            #components = len(alpha)
+            #component_idx = np.random.choice(components, size=N_samples, p=alpha)
+            #samples = np.random.logistic(loc=model[component_idx], scale=beta[component_idx], size=N_samples)
         else:
             # Sample from a discretized mixture logistic distribution
-            component_indices = np.random.choice(len(alpha), size=N_samples, p=alpha)
-            component_probabilities = 1 / (1 + np.exp(-beta * model))
-            component_samples = np.random.binomial(1, component_probabilities)
-            samples = np.where(component_samples == 1, np.random.logistic(scale=beta * model), threshold)
-
+            #components = len(alpha)
+            #component_idx = np.random.choice(components, size=N_samples, p=alpha)
+            #uniform_samples = np.random.uniform(size=N_samples)
+            #samples = model[component_idx] + beta[component_idx] * np.log(uniform_samples / (1 - uniform_samples))
+            #samples = np.where(samples < threshold, samples, threshold)
+            
     return samples

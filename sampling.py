4be45a81d5dd059fa61a80850ab8d5f180f4202a
ludovicabrusaferri <loginvarious12@gmail.com>
2023-07-03 11:44:56 -0400

    sampling

--- sampling.py
+++ sampling.py
@@ -1,26 +1,44 @@
 import numpy as np
 
-def sampling_function(meas, mean, sigma, N_samples, alpha=None, threshold=None):
+def sample_from_logistic_distribution(location, scale, number_of_samples, alpha=None, threshold=None):
+    location = np.asarray(location)
+    scale = np.asarray(scale)
+
     if alpha is None:
-        # Generate N_samples from a logistic distribution
-        samples = np.random.logistic(loc=mean, scale=sigma, size=N_samples)
-        return samples
-        
+        # Sample from a logistic distribution
+        samples = np.random.logistic(loc=location, scale=scale, size=number_of_samples)
     else:
+        alpha = np.asarray(alpha)
+
         if threshold is None:
             # Sample from a mixture of logistic distributions
-            component_means = mean  # Mean for each component
-            component_sigmas = sigma  # Standard deviation for each component
+            components = len(alpha)
 
-            # Choose component based on alpha probabilities
-            components = np.random.choice(len(component_means), size=N_samples, p=alpha)
+            samples = np.zeros(number_of_samples)
+            component_indices = np.random.choice(components, size=number_of_samples, p=alpha)
 
-            # Sample from chosen components
-            samples = np.zeros(N_samples)
-            for i in range(len(component_means)):
-                component_indices = np.where(components == i)[0]
-                component_samples = np.random.logistic(loc=component_means[i], scale=component_sigmas[i], size=len(component_indices))
-                samples[component_indices] = component_samples
+            for i in range(components):
+                component_mask = (component_indices == i)
+                component_samples = np.random.logistic(loc=location[i], scale=scale[i], size=np.sum(component_mask))
+                samples[component_mask] = component_samples
 
-            return samples
+        else:
+            # Sample from a discretised mixture logistic distribution
+            threshold = np.asarray(threshold)
+            components = len(alpha)
+
+            samples = np.zeros(number_of_samples)
+            component_indices = np.random.choice(components, size=number_of_samples, p=alpha)
+            uniform_samples = np.random.uniform(size=number_of_samples)
+
+            for i in range(components):
+                component_mask = (component_indices == i)
+                component_samples = location[i] + (scale[i] * np.log(uniform_samples[component_mask] / (1 - uniform_samples[component_mask])))
+                samples[component_mask] = component_samples
+
+            samples = np.where(samples < threshold, samples, threshold)
+
+    sample = np.mean(samples, axis=0)
+
+    return sample
 

e827cede2b8f1ded4ac0a323e99e503bb0e4cab8
ludovicabrusaferri <loginvarious12@gmail.com>
2023-07-10 14:13:16 -0400

    l

--- gaussianpython.py
+++ gaussianpython.py
@@ -18,12 +18,8 @@ dtype = tf.float32
    # return samples
 
 def discretized_gaussian_negative_log_likelihood_loss(data, mu, sigma, Mlow, Mup):
+    data_i = np.clip(data, Mlow, Mup)
     log_likelihood = tf.reduce_sum(-0.5 * ((data - mu)**2 / (2*sigma**2) + tf.math.log(tf.sqrt(2*np.pi)*sigma)))
-    cdf_Mlow = norm.cdf(Mlow, mu, sigma)
-    cdf_Mup = norm.cdf(Mup, mu, sigma)
-    correction_factor = 1 - (cdf_Mup - cdf_Mlow)
-
-    log_likelihood = log_likelihood + tf.math.log(correction_factor)
     return -log_likelihood
 
 def sample_from_discretized_gaussian_distribution(mu, sigma, num_samples, Mlow, Mup):
